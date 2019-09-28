d45cda9aa387c2f633976d67131d55dd18789fb9
Sam Lukas <iggyboy456@gmail.com>
2019-09-28 15:05:58 -0500

    added functionalilty for most functions

--- liri.js
+++ liri.js
@@ -14,6 +14,18 @@ if (whatFunc === "concert-this") {
     console.log("running bands function");
     bands(value);
 }
+else if (whatFunc === "spotify-this") {
+    console.log("running spotify function");
+    spotifyThis(value);
+}
+else if (whatFunc === "movie-this"){
+    console.log("running omdb function");
+    movieThis(value);
+}
+else if (whatFunc === "do-what-it-says"){
+    console.log("doing the thing");
+    doIt(value);
+}
 
 function bands(artist) {
     axios
@@ -43,11 +55,73 @@ function bands(artist) {
         });
 }
 function spotifyThis(song) {
-
+    spotify
+        .search({ type: 'track', query: song })
+        .then(function (response) {
+            for (var i = 0; i < response.tracks.items.length; i++) {
+                console.log("Artist: " + response.tracks.items[i].album.artists[0].name);
+                console.log("Title: " + response.tracks.items[i].name);
+                console.log("Preview: " + response.tracks.items[i].preview_url);
+                console.log("Album: " + response.tracks.items[i].album.name);
+                console.log("-------------------------");
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
 function movieThis(movie) {
-
+    axios
+        .get("http://www.omdbapi.com/?t="+movie+"&y=&plot=short&apikey=trilogy")
+        .then(function (response) {
+            console.log("Title: " + response.data.Title);
+            console.log("Year: " + response.data.Year);
+            console.log("IMDB Rating: "+response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating: "+response.data.Ratings[1].Value);
+            console.log("Country: "+response.data.Country);
+            console.log("Language: "+response.data.Language);
+            console.log("Plot: "+response.data.Plot);
+            console.log("Actors: "+response.data.Actors);
+            console.log("-------------------------"); 
+        })
+        .catch(function (error) {
+            if (error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            }
+            else if (error.request) {
+                console.log(error.request);
+            }
+            else {
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
 }
 function doIt() {
-
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        console.log(data);
+        let dataArr = data.split(",");
+        console.log(dataArr);
+        if (dataArr[0] === "concert-this") {
+            console.log("running bands function");
+            bands(dataArr[1]);
+        }
+        else if (dataArr[0] === "spotify-this") {
+            console.log("running spotify function");
+            spotifyThis(dataArr[1]);
+        }
+        else if (dataArr[0] === "movie-this"){
+            console.log("running omdb function");
+            movieThis(dataArr[1]);
+        }
+        else if (dataArr[0] === "do-what-it-says"){
+            console.log("doing the thing");
+            doIt(dataArr[1]);
+        }
+        else {
+            console.log("bruh");
+        }
+    });
 }
